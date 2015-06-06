922fad0c7133d4b6e4ccd0d5a91209e14648c092
AlexanderPower <aleksandr.power88@gmail.com>
2015-06-06 22:03:20 +0500

    Added header.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :collections
   devise_for :users
+  root 'collections#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
