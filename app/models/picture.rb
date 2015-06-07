8b5fe2c67fd55ff3174baeab0fde05bad8cae3b1
AlexanderPower <aleksandr.power88@gmail.com>
2015-06-07 20:05:56 +0500

    Added paperclip and picture model.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,5 +2,5 @@ class Picture < ActiveRecord::Base
   belongs_to :collection
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :links
   resources :collections
+  resources :pictures
   devise_for :users
   root 'collections#index'
   # The priority is based upon order of creation: first created -> highest priority.
